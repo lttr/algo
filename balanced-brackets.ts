9b6692e85142cfffdf636e552575e279287f2abe
Lukas Trumm <lukas.trumm@gmail.com>
2023-01-24 12:23:56 +0100

    Add algo balanced-brackets

---
+++ balanced-brackets.ts
@@ -0,0 +1,38 @@
+// Turing.com - practice
+
+// Input: exp = “[()]{}{[()()]()}”
+// Output: Balanced
+// Explanation: all the brackets are well-formed
+//
+// Input: exp = “[(])”
+// Output: Not Balanced
+// Explanation: 1 and 4 brackets are not balanced because
+// there is a closing ‘]’ before the closing ‘(‘
+
+export function isBalanced(input: string) {
+  const stack: string[] = [];
+  for (const char of input) {
+    const last = stack.at(-1);
+    if (last) {
+      if (
+        (char === "]" && last === "[") ||
+        (char === ")" && last === "(") ||
+        (char === "}" && last === "{")
+      ) {
+        stack.pop();
+      } else {
+        stack.push(char);
+      }
+    } else {
+      stack.push(char);
+    }
+    if (import.meta.main) {
+      console.log(stack);
+    }
+  }
+  return stack.length === 0;
+}
+
+if (import.meta.main) {
+  isBalanced("(((){}))");
+}
---
+++ balanced-brackets_test.ts
@@ -0,0 +1,18 @@
+import { assertEquals } from "https://deno.land/std@0.173.0/testing/asserts.ts";
+import { isBalanced } from "./balanced-brackets.ts";
+
+Deno.test(function test1() {
+  assertEquals(isBalanced("()"), true);
+});
+
+Deno.test(function test2() {
+  assertEquals(isBalanced("({[]})"), true);
+});
+
+Deno.test(function test3() {
+  assertEquals(isBalanced("({][})"), false);
+});
+
+Deno.test(function test4() {
+  assertEquals(isBalanced("{(})"), false);
+});
